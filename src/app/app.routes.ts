4c43c45af179a4f52fd6260e1e1b8390a8923e46
Eduardo Araujo <eduardoaraujo6660@example.com>
2025-03-12 21:03:04 -0500

    Creacion de componentes de paginas Informacion,Catalogo, Ofertas y Ruteo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { InformacionComponent } from './informacion/informacion.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { OfertasComponent } from './ofertas/ofertas.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'app', component: AppComponent }, // Página de inicio
+  { path: 'informacion', component: InformacionComponent },
+  { path: 'catalogo', component: CatalogoComponent },
+  { path: 'ofertas', component: OfertasComponent },
+];
---
+++ src/app/informacion/informacion.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-informacion',
+  templateUrl: './informacion.component.html',
+  standalone: true,
+})
+export class InformacionComponent {}
