8247ac9b2b8bb18ee252fee6560654e9999b804f
Eduardo Araujo <eduardoaraujo6660@example.com>
2025-03-15 13:42:46 -0500

    Conexion al BackEnd y Correcion de espacio de datos de productos en Inicio.componente.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';  //Importamos el RouterModule
 import { FooterComponent } from './footer/footer.component';  // Importamos el Footer
-
+import { ProductosService } from './services/productos.service';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterModule, FooterComponent],  // Agregammos el Footer/RouteR
   templateUrl: './app.component.html',  // Apuntando al archivo correcto del html donde se encuentran los componentes globalizados
-  styleUrls: ['./app.component.css']//Apuntando al archivo css donde estan los estilados de los componentes globalizados
+  styleUrls: ['./app.component.css'],//Apuntando al archivo css donde estan los estilados de los componentes globalizados
+  providers: [ProductosService]
 })
 export class AppComponent {
   title = 'SweetShop';
+  productos: any[]=[]
+
+  constructor(private productosService: ProductosService){}
+
+  ngOnInit(){
+    this.productosService.getProductos().subscribe(data=>{
+      this.productos = data;
+      console.log('📦 Productos cargados: ', this.productos);
+    });
+  }
 }
--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';  // <-- Importamos el modulo router
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-inicio',
   standalone: true,
@@ -9,5 +9,19 @@ import { RouterModule } from '@angular/router';  // <-- Importamos el modulo rou
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {}
+export class InicioComponent {
+  private http = inject(HttpClient);
+  productos: any[]=[];
+
+  constructor(){
+    this.cargarProductos();
+  }
+
+  cargarProductos(){
+    this.http.get<any[]>('http://localhost:3000/productos').subscribe({
+      next:(data)=>(this.productos=data),
+      error:(err)=>console.error('Error al obtener productos',err),
+    });
+  }
+}
 
