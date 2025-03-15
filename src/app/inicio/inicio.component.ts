59e5b8665ba1224aa9a2e6b78219ac2722b36341
Eduardo Araujo <eduardoaraujo6660@example.com>
2025-03-15 14:49:37 -0500

    Arreglo de diseño de simbolo de precios en inicio.component.html

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,27 +1,31 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';  // <-- Importamos el modulo router
+import { RouterModule } from '@angular/router';  
 import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CommonModule, RouterModule],  // <-- Aquí pongo el modulo de router
+  imports: [CommonModule, RouterModule],  
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
   private http = inject(HttpClient);
-  productos: any[]=[];
+  productos: any[] = [];
 
-  constructor(){
+  constructor() {
     this.cargarProductos();
   }
 
-  cargarProductos(){
+  cargarProductos() {
     this.http.get<any[]>('http://localhost:3000/productos').subscribe({
-      next:(data)=>(this.productos=data),
-      error:(err)=>console.error('Error al obtener productos',err),
+      next: (data) => (this.productos = data),
+      error: (err) => console.error('Error al obtener productos', err),
     });
   }
 }
+
+
+
 
