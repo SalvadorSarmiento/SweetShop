4d57a908a34d06f045498355d30b9e6c627e7aff
Eduardo Araujo <eduardoaraujo6660@example.com>
2025-03-19 10:13:56 -0500

    Convirtiendo header en un componente globalizado as Titulo-principal.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TituloPrincipalComponent } from './titulo-principal/titulo-principal.component';
 import { RouterModule } from '@angular/router';  //Importamos el RouterModule
 import { FooterComponent } from './footer/footer.component';  // Importamos el Footer
 import { ProductosService } from './services/productos.service';
@@ -8,7 +9,7 @@ import { BarraLateralComponent } from './barra-lateral/barra-lateral.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, FooterComponent, BarraLateralComponent],  // Agregammos el Footer/RouteR
+  imports: [CommonModule, TituloPrincipalComponent, RouterModule, FooterComponent, BarraLateralComponent],  // Agregammos el Footer/RouteR
   templateUrl: './app.component.html',  // Apuntando al archivo correcto del html donde se encuentran los componentes globalizados
   styleUrls: ['./app.component.css'],//Apuntando al archivo css donde estan los estilados de los componentes globalizados
   providers: [ProductosService]
--- src/app/informacion/informacion.component.ts
+++ src/app/informacion/informacion.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-informacion',
   templateUrl: './informacion.component.html',
+  styleUrl: './informacion.component.css',
   standalone: true,
 })
 export class InformacionComponent {}
